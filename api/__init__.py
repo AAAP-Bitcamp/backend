c7b80e932ac9b06ed737c2683fa7eb096854d4cd
averma99 <amalverma99@gmail.com>
2023-04-07 22:13:31 -0400

    Connect to cockroachdb

--- api/__init__.py
+++ api/__init__.py
@@ -5,6 +5,7 @@ from flask_socketio import SocketIO
 
 cors = CORS()
 socketio = SocketIO(cors_allowed_origins='*')
+movr = None
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -12,5 +13,8 @@ def create_app(config_class=Config):
 
     cors.init_app(app)
     socketio.init_app(app)
+
+    from api.movr import MovR
+    movr = MovR(app.config.get('DATABASE_URL'))
 
     return app
--- main.py
+++ main.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, redirect, url_for
 from flask_socketio import SocketIO
 
+import string
+import secrets
+from pydantic import BaseModel
+from flask_pydantic import validate
+
 from api import create_app, socketio
+
+# class Room(BaseModel):
+#     ID: string
+#     admin_id: string
+
+# class User(BaseModel):
+#     ID: string
+#     name: string
+#     image: string
+#     room_id: string
 
 app = create_app()
 
 if __name__ == '__main__':
     socketio.run(app)
+
+@app.route('/')
+def index():
+    return "<p>Home Page</p>"
+
+@app.route('/room/create')
+def create():
+    return {
+        'room_code': ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(7))
+    }
+
+@app.route('/room/:room_code')
+def create_room(room_code: string):
+    # store image in cockroachdb
+    return "add return here"
