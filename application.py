b32d71c39c30dcf6cccb480dd93bde2ea9389e26
alemtani <alex.m.tanimoto@gmail.com>
2023-04-08 15:44:43 -0400

    Rename application attribute?

--- application.py
+++ application.py
@@ -1,19 +1,6 @@
-from pydantic import BaseModel
-from flask_pydantic import validate
-
 from api import create_app, socketio
 
-# class Room(BaseModel):
-#     ID: string
-#     admin_id: string
-
-# class User(BaseModel):
-#     ID: string
-#     name: string
-#     image: string
-#     room_id: string
-
-app = create_app()
+application = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(application, debug=True)
