2647160dbf2af392258813763f2143ab78f9579e
alemtani <alex.m.tanimoto@gmail.com>
2023-04-08 20:05:00 -0400

    Debugging on join

--- api/sockets.py
+++ api/sockets.py
@@ -8,14 +8,22 @@ sockets = Blueprint('sockets', __name__)
 def on_join(data):
     user_id = data['user_id']
     room_code = data['room_code']
+    print('Getting user')
     user = movr.get_user(user_id)
+    print('Getting room')
     room = movr.get_room(room_code)
-    if user and room:
+    if user and room and room.creator != user:
+        print('Do add room user')
         movr.add_room_user(room.id, user.id)
+        print('Do get users')
         users = movr.get_users(room.id)
+        print('Do join room')
         join_room(room)
+        print('Send')
         send(users, to=room)
     else:
+        print(user)
+        print(room)
         send('error', f'Could not join room: {room_code}')
 
 @socketio.on('leave')
