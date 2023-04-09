97b73fa58eded5ba74d0e0df1b6da7011fbb5eeb
alemtani <alex.m.tanimoto@gmail.com>
2023-04-09 03:49:43 -0400

    Remove room=

--- api/sockets.py
+++ api/sockets.py
@@ -14,8 +14,8 @@ def on_join(data):
     if user and room:
         movr.add_room_user(room.id, user.id)
         users = movr.get_users(room.id)
-        join_room(room_code)
-        emit('join', users, room=room_code, to=room_code)
+        join_room(room.id)
+        emit('join', users, to=room.id)
     else:
         emit('join', {'error': f'Could not join room: {room_code}'})
         
@@ -32,7 +32,7 @@ def on_start(data):
         user_data = {user['name'] : user['score'] for user in users}
         print('Done user data')
         print(request.sid)
-        emit('start', user_data, room=room_code, broadcast=False, to=room_code)
+        emit('start', user_data, broadcast=False, to=room.id)
     else:
         emit('start', {'error': f'Could not start game in room: {room_code}'}, room=room_code,broadcast=False)
 
